e82ea57e44a9da455403597ae64e7ca5a6cca473
Mazen Saaed <80298280+MazenSaaed11@users.noreply.github.com>
2024-06-20 23:08:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,102 @@
+import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
+import model
+
+# loading yolov3 model
+weights_path = "yolov3.weights"
+config_path = "yolov3.cfg"
+names_path = "coco.names"
+net, output_layers, classes = model.load_yolo(weights_path, config_path, names_path)
+
+# layout of the web app
+icon, title = st.columns([1, 5])
+
+with icon:
+    st.image("appIcon.png", width=100)
+
+with title:
+    st.title("Object Detector Web App")
+
+st.markdown('---')
+
+user_image, confidence = st.columns([6, 6])
+input_image = user_image.file_uploader("Please upload an image to start detecting...", type=["PNG", "JPEG", "JPG"])
+threshold = confidence.slider(label='Confidence Threshold(%):', min_value=0, max_value=100, step=1, value=80)
+confidence.caption('Move the slider to choose how certain you want the app to be when detecting objects.')
+
+image_placeholder = st.empty()
+if input_image is not None:
+    image_placeholder.image(input_image)
+
+show_button = input_image is not None
+
+identify_btn = st.empty()
+result = st.empty()
+show_boxed_image = st.empty()
+boxed_image_placeholder = st.empty()
+
+if 'previous_image' not in st.session_state:
+    st.session_state.previous_image = None
+
+if 'show_boxed_image' not in st.session_state:
+    st.session_state.show_boxed_image = False
+
+if 'show_results' not in st.session_state:
+    st.session_state.show_results = False
+
+def identify_btn_clicked():
+    st.session_state.show_results = True
+
+
+def show_boxed_image_clicked():
+    st.session_state.show_boxed_image = True
+
+
+
+if input_image is None or input_image != st.session_state.previous_image:
+    st.session_state.previous_image = input_image
+    st.session_state.show_boxed_image = False
+    st.session_state.show_results = False
+
+if show_button:
+    identify_btn.button('Identify the objects', on_click=identify_btn_clicked)
+    show_boxed_image.button('Show image with objects in boxes', on_click=show_boxed_image_clicked)
+
+if st.session_state.show_results:
+    result.image("waiting.gif")
+    image = Image.open(input_image)
+    img_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    detected_objects = model.detect_objects(img_cv, net, output_layers, classes, confidence_threshold=threshold / 100.0)
+
+    if detected_objects:
+        detected_info = "### Objects detected:\n"
+        for obj in detected_objects:
+            detected_info += f"- **Class:** {obj['class']}, **Confidence:** {obj['confidence']:.2f}, **Box:** {obj['box']}\n"
+        result.markdown(detected_info)
+
+if st.session_state.show_boxed_image:
+    boxed_image_placeholder.image("waiting.gif")
+    image = Image.open(input_image)
+    img_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    annotated_image = model.annotate_image(img_cv, net, output_layers, classes, confidence_threshold=threshold / 100.0)
+    annotated_image_rgb = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+    annotated_image_pil = Image.fromarray(annotated_image_rgb)
+    boxed_image_placeholder.image(annotated_image_pil)
+
+st.markdown('---')
+
+github_icon, github_link, linkedin_icon, linkedin_link = st.columns([1, 5, 1, 5])
+
+with github_icon:
+    st.image("github.png", width=30)
+
+with github_link:
+    st.markdown("[My GitHub](https://github.com/MazenSaaed11)")
+
+with linkedin_icon:
+    st.image("linkedin.png", width=30)
+
+with linkedin_link:
+    st.markdown("[My LinkedIn](https://www.linkedin.com/in/mazen-saaed-383247235/)")
---
+++ model.py
@@ -0,0 +1,70 @@
+import cv2
+import numpy as np
+
+
+def load_yolo(weights_path, config_path, names_path):
+    net = cv2.dnn.readNet(weights_path, config_path)
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    with open(names_path, "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+
+    return net, output_layers, classes
+
+
+def detect_objects(image, net, output_layers, classes, confidence_threshold, nms_threshold=0.4):
+    height, width, channels = image.shape
+
+    blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence >= confidence_threshold:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, confidence_threshold, nms_threshold)
+
+    detected_objects = []
+    for i in indexes:
+        x, y, w, h = boxes[i]
+        label = str(classes[class_ids[i]])
+        confidence = confidences[i]
+        detected_objects.append({"class": label, "confidence": confidence * 100, "box": (x, y, x + w, y + h)})
+
+    return detected_objects
+
+
+def annotate_image(image, net, output_layers, classes, confidence_threshold=0.5, nms_threshold=0.4, font_thickness=2):
+    detected_objects = detect_objects(image, net, output_layers, classes, confidence_threshold, nms_threshold)
+    font = cv2.FONT_HERSHEY_PLAIN
+    font_scale = 1
+    annotated_image = image.copy()
+
+    for obj in detected_objects:
+        label = obj["class"]
+        confidence = obj["confidence"]
+        x1, y1, x2, y2 = obj["box"]
+        color = (0, 0, 255)
+        text_color = (0, 255, 0)
+        cv2.rectangle(annotated_image, (x1, y1), (x2, y2), color, 2)
+        cv2.putText(annotated_image, f"{label}: {confidence:.2f}%", (x1, y1 - 5), font, font_scale, text_color,
+                    font_thickness)
+
+    return annotated_image
